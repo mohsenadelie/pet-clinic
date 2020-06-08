83fcafb807d1ee7e74853ed29eab73c7d29d11d1
Mohsen Adeli <mohsen.adelie@gmail.com>
2020-06-09 00:31:34 +0430

    Implement interface for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/adeli/services/PetService.java
@@ -0,0 +1,15 @@
+package com.adeli.services;
+
+import com.adeli.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
