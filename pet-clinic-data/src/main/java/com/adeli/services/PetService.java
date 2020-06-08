0d12409937b0feffb38d8b03224ff9b310a3c7af
Mohsen Adeli <mohsen.adelie@gmail.com>
2020-06-09 03:07:42 +0430

    refactoring to common crud interface. closes #11.

--- pet-clinic-data/src/main/java/com/adeli/services/PetService.java
+++ pet-clinic-data/src/main/java/com/adeli/services/PetService.java
@@ -2,14 +2,6 @@ package com.adeli.services;
 
 import com.adeli.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
