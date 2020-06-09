498c6316d36410d3546cad87811067bcfe68e7fd
Mohsen Adeli <mohsen.adelie@gmail.com>
2020-06-10 02:21:05 +0430

    Impl Map based Services. closes #6.

---
+++ pet-clinic-data/src/main/java/com/adeli/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.adeli.services.map;
+
+import com.adeli.model.Owner;
+import com.adeli.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
