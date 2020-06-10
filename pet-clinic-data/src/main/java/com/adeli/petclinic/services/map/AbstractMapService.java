d267454dfb32b828c893fa4569f38cdf341f0b1c
Mohsen Adeli <mohsen.adelie@gmail.com>
2020-06-11 03:07:49 +0430

    Refactored Map Impl to abstract ID generation. closes #23.

--- pet-clinic-data/src/main/java/com/adeli/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/adeli/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,14 @@
 package com.adeli.petclinic.services.map;
 
+import com.adeli.petclinic.model.BaseEntity;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +18,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -28,5 +38,11 @@ public abstract class AbstractMapService<T, ID> {
 
     void deleteById(ID id) {
         map.remove(id);
+    }
+
+    private Long getNextId() {
+        return map.keySet().stream()
+                .max(Long::compare)
+                .orElse(1L) + 1;
     }
 }
